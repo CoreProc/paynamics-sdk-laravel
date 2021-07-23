22af02679d64fa5dbd4ab7e2b6ca44c2c0a92011
vagrant <vagrant@homestead>
2021-07-23 06:33:49 +0000

    Fix request

--- src/Services/Clients/PostClient.php
+++ src/Services/Clients/PostClient.php
@@ -2,26 +2,33 @@
 
 namespace Coreproc\PaynamicsSdk\Services\Clients;
 
-use Coreproc\PaynamicsSdk\Services\PaymentService;
+use Coreproc\PaynamicsSdk\PaynamicsClient;
+use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Client;
 
 class PostClient
 {
     /**
      * PostClient constructor.
+     * @throws GuzzleException
      */
-    public function __construct()
+    public function __construct(array $request)
     {
+        $paynamicsClient = app(PaynamicsClient::class);
+        $client = new Client();
 
+        $client->request('POST', $paynamicsClient->getEndpoint(), ['form_params' => $request]);
     }
 
     /**
      * Create new instance of post client
      *
+     * @param array $request
      * @return PostClient
+     * @throws GuzzleException
      */
-    public static function make(): PostClient
+    public static function make(array $request): PostClient
     {
-        return new self();
+        return new self($request);
     }
 }
