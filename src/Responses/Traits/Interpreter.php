69d0cafd1d73ee99179b9e7a3d5ea403e97ec84a
John Lemuel G. Hapa <johnlemuel.hapa@coreproc.ph>
2021-08-02 10:11:15 +0800

    Response Interpreter

--- src/Responses/Traits/Interpreter.php
+++ src/Responses/Traits/Interpreter.php
@@ -2,10 +2,41 @@
 
 namespace Coreproc\PaynamicsSdk\Responses\Traits;
 
+use Exception;
+use SimpleXMLElement;
+
 trait Interpreter
 {
-    public function interpret()
+    /**
+     * @param $response
+     * @return string
+     * @throws Exception
+     */
+    public function interpret($response): string
     {
-        //
+//        return base64_decode($response);
+        return $this->toXml($response);
+//        return $this->toArray($response);
+//        return $this->toArray($this->toXml($response));
+    }
+
+    /**
+     * @param string $response
+     * @return string
+     * @throws Exception
+     */
+    private function toXml(string $response): string
+    {
+        return (new SimpleXMLElement(base64_decode($response)));
+    }
+
+    /**
+     * @param $xml
+     * @return array
+     */
+    private function toArray($xml): array
+    {
+        $json = json_encode(simplexml_load_string($xml, "SimpleXMLElement", LIBXML_NOCDATA));
+        return json_decode($json,TRUE);
     }
 }
